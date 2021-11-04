36f48c03ae7deb850b1695f366f413d4c84b233a
Gyozo55 <74531373+Gyozo55@users.noreply.github.com>
2021-11-04 10:03:12 +0100

    [#1] Create display() and getNumberBetween() methods in ConsoleView.java

--- src/main/java/com/codecool/gladiator/Main.java
+++ src/main/java/com/codecool/gladiator/Main.java
@@ -3,13 +3,18 @@ package com.codecool.gladiator;
 import com.codecool.gladiator.controller.Colosseum;
 import com.codecool.gladiator.model.gladiators.GladiatorFactory;
 import com.codecool.gladiator.view.ConsoleView;
+import com.codecool.gladiator.view.Viewable;
+
+import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+//        ConsoleView consoleView = new ConsoleView();
         Colosseum colosseum = new Colosseum(new ConsoleView(), new GladiatorFactory("Names.txt"));
         colosseum.welcomeAndAskForStages();
         colosseum.runSimulation();
+//        consoleView.display("test");
     }
 
 }
